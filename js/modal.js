61264a1b249918259ac4ff218c6ddc6a644d0dc3
Anzhelika-Light <anzhelikaantoniuk1202@gmail.com>
2023-07-02 14:14:51 +0300

    adaptive header, about, services sections are done

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,14 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    body: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    refs.body.style.overflow = refs.modal.classList.contains('is-hidden') ? 'hidden' : 'auto';
     refs.modal.classList.toggle('is-hidden');
   }
 })();
